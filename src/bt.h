b36e30cb2d0304dd3609a25eae313b9b04bac8ca
Cliff(dlktdr) <cblackburn320@gmail.com>
2021-09-19 23:59:39 -0700

    Some BT Constants from OTX

--- src/bt.h
+++ src/bt.h
@@ -1,3 +1,4 @@
+#pragma once
 /*
    This example code is in the Public Domain (or CC0 licensed, at your option.)
 
@@ -11,6 +12,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+const uint8_t START_STOP    = 0x7E;
+const uint8_t BYTE_STUFF    = 0x7D;
+const uint8_t STUFF_MASK    = 0x20;
+const uint8_t TRAINER_FRAME = 0x80;
 
 /* Attributes State Machine */
 enum
